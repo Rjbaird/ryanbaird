e86613841a9b6b335104b7e3a58799871bf1c540
Rjbaird <rjbaird09@gmail.com>
2021-09-05 19:51:51 -0500

    added coinbase ynab sync script

--- src/components/ProjectsCard.js
+++ src/components/ProjectsCard.js
@@ -5,12 +5,12 @@ const ProjectsCard = (props) => {
         <div className="relative py-16 bg-black">
             <div className="hidden absolute top-0 inset-x-0 h-1/2 bg-black lg:block" aria-hidden="true"></div>
             <div className="max-w-7xl mx-auto bg-indigo-600 lg:bg-transparent lg:px-8">
-                <div className="lg:grid lg:grid-cols-12">
-                    <div className="relative z-10 lg:col-start-1 lg:row-start-1 lg:col-span-4 lg:py-16 lg:bg-transparent">
+                <div className="lg:grid lg:grid-cols-9 lg:grid-row-9">
+                    <div className="relative z-10 lg:col-start-1 lg:row-start-1 lg:col-span-4 lg:py-4 lg:bg-transparent">
                         <div className="absolute inset-x-0 h-1/2 bg-black lg:hidden" aria-hidden="true"></div>
                         <div className="max-w-md mx-auto px-4 sm:max-w-3xl sm:px-6 lg:max-w-none lg:p-0">
-                            <div className="aspect-w-10 aspect-h-6 sm:aspect-w-2 sm:aspect-h-1 lg:aspect-w-1">
-                                <img className="object-cover object-center rounded-3xl shadow-2xl" src={props.src} alt=""/>
+                            <div className="aspect-w-10 aspect-h-6 sm:aspect-w-2 sm:aspect-h-1 lg:aspect-w-2">
+                                <img className="lg:my-24 object-cover object-center rounded-3xl shadow-2xl" src={props.src} alt=""/>
                             </div>
                         </div>
                     </div>
@@ -33,8 +33,8 @@ const ProjectsCard = (props) => {
                                 <rect width="404" height="384" fill="url(#64e643ad-2176-4f86-b3d7-f2c5da3b6a6d)" />
                             </svg>
                         </div>
-                        <div className="relative max-w-md mx-auto py-12 px-4 space-y-6 sm:max-w-3xl sm:py-16 sm:px-6 lg:max-w-none lg:p-0 lg:col-start-4 lg:col-span-6">
-                            <h2 className="sm:text-4xl lg:text-5xl text-center font-extrabold text-white" id="join-heading">{props.title}</h2>
+                        <div className="relative max-w-md mx-2 py-12 px-4 space-y-6 sm:max-w-3xl sm:py-8 md:py-20 lg:py-24 sm:px-6 lg:max-w-none lg:p-0 lg:col-start-4 lg:col-span-6">
+                            <h2 className="text-4xl lg:text-5xl text-center font-extrabold text-white" id="join-heading">{props.title}</h2>
                             <p className="mt-8 border-t border-gray-200 pt-6 text-2xl text-white">{props.data}</p>
                                 <div>
                                     <p className="text-xl text-white">
