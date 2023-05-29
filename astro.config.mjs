b79df9d8eb00450f62146988958b044b118d24e4
Rjbaird <rjbaird09@gmail.com>
2023-05-29 00:16:40 -0500

    fix: typo in rss causing build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,14 @@
 import mdx from "@astrojs/mdx";
 import netlify from "@astrojs/netlify/functions";
-import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import { remarkReadingTime } from "./remark-reading-time.mjs";
 
 // https://astro.build/config
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://bairrya-blog.netlify.app",
-  integrations: [tailwind(), mdx({
-    remarkPlugins: [remarkReadingTime]
-  }), solidJs(), svelte()],
-  adapter: netlify()
+    site: "https://bairrya-blog.netlify.app",
+    integrations: [tailwind(), mdx(), svelte()],
+    // adapter: netlify(),
 });
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,10 +2,11 @@ import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 
 export async function get(context) {
-    const blogs = await getCollection("blogs");
+    const blogs = await getCollection("blog");
     return rss({
         title: "Ryan Baird’s Blog",
-        description: "A guy trying to figure out how this internet thing works.",
+        description:
+            "A guy trying to figure out how this internet thing works.",
         site: context.site,
         items: blogs.map((blog) => ({
             title: blog.data.title,
@@ -13,8 +14,8 @@ export async function get(context) {
             description: blog.data.description,
             customData: blog.data.customData,
             // Compute RSS link from blog `slug`
-            // This example assumes all blogs are rendered as `/blogs/[slug]` routes
-            link: `/blogs/${blog.slug}/`,
+            // This example assumes all blog are rendered as `/blog/[slug]` routes
+            link: `/blog/${blog.slug}/`,
         })),
     });
 }
