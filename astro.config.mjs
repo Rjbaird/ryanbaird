42174edc506dd80f4d001d269acdb7768a10b4c8
Rjbaird <rjbaird09@gmail.com>
2024-03-08 10:39:26 -0600

    add prefetch + update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,7 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   site: "https://bairrya-blog.netlify.app",
   integrations: [tailwind(), svelte(), mdx()],
+  prefetch: {
+    prefetchAll: true,
+  },
 });
